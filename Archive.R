dab86489139e0cf71e4d825691d3dc1470972d5f
Showhand <showhandss@gmail.com>
2021-03-04 16:43:52 +0800

    import data

---
+++ Archive.R
@@ -0,0 +1,15 @@
+library(readr)
+Cheddar40Sales <- read_csv("Datamart-Export_DY_WK100-40 Pound Block Cheddar Cheese Prices and Sales_20170829_122601.csv")
+View(Cheddar40Sales)
+
+Cheddar100Sales <- read_csv("Datamart-Export_DY_WK100-500 Pound Barrel Cheddar Cheese Prices, Sales, and Moisture Content_20170829_122601.csv")
+View(Cheddar100Sales)
+
+Butter100Sales <- read_csv("Datamart-Export_DY_WK100-Butter Prices and Sales_20170829_122601.csv")
+View(Butter100Sales)
+
+DryWheySales <- read_csv("Datamart-Export_DY_WK100-Dry Whey Prices and Sales_20170829_122601.csv")
+View(DryWheySales)
+
+NonfatDryMilkSales <- read_csv("Datamart-Export_DY_WK100-Nonfat Dry Milk Prices and Sales_20170829_122601.csv")
+View(NonfatDryMilkSales)
